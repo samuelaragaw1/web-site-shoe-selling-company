c213902414eada07fd5540f9842ad81f8c7bb804
samuelaragaw1 <samuelaragaw87@gmail.com>
2025-09-23 23:02:53 +0300

    finished the navigation bar

--- Main.js
+++ Main.js
@@ -1,8 +1,23 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     let scrolled = 0;
+    let fav_click = 0;
     console.log("Document loaded");
     console.log(document.documentElement.scrollHeight);
     document.addEventListener('scroll', scrollFunction);
+    document.querySelector("#favorite").addEventListener('click', 
+            function (e) {
+                e.preventDefault();
+                let counter = document.querySelector("#number_favorite");
+                fav_click++;
+                console.log(fav_click);
+                counter.innerHTML = fav_click;
+                if (fav_click >= 10) {
+                    counter.style.width = '1.6rem';
+                    counter.style.paddingLeft = '0.7%'
+                }
+    }
+    );
     function scrollFunction() {
         const scroll_Y = window.scrollY;
         if (scroll_Y > scrolled) {
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+first: 
+  - make sure make images for <nav> 
+  - make the navigation bar working
