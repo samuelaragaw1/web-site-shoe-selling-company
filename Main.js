1727d5816d319e0ecf9bb95085d805b12691be0b
samuelaragaw1 <samuelaragaw87@gmail.com>
2025-09-24 17:08:00 +0300

    fix some issues

--- Main.js
+++ Main.js
@@ -10,22 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
                 e.preventDefault();
                 let counter = document.querySelector("#number_favorite");
                 fav_click++;
-                console.log(fav_click);
                 counter.innerHTML = fav_click;
                 if (fav_click >= 10) {
                     counter.style.width = '1.6rem';
-                    counter.style.paddingLeft = '0.7%'
+                    counter.style.paddingLeft = '0.7%';
                 }
     }
     );
     function scrollFunction() {
-        const scroll_Y = window.scrollY;
+        let header = document.querySelector('header');
+        let scroll_Y = window.scrollY;
+        console.log(scroll_Y)
+
+        /*this for cheaking if scrolled down*/
         if (scroll_Y > scrolled) {
             scrolling_down = true;
         } else {
             scrolling_down = false;
         }
         scrolled = scroll_Y;
+
+        /*this for navigation*/
         if (scrolling_down) {
             document.querySelector('nav').classList.add('hidden');
             document.querySelector('nav').classList.remove('visible');
@@ -33,6 +38,14 @@ document.addEventListener('DOMContentLoaded', function() {
         else {
             document.querySelector('nav').classList.add('visible');
             document.querySelector('nav').classList.remove('hidden');
+        }
+
+        /*Making header fixed*/
+        if (scroll_Y >= 800) {
+            header.style.top = scroll_Y - 800 + 'px';
+        }
+        else {
+            header.style.top = "0px";
         }
     }
 });
