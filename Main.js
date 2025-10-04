8554d6e1dab28256c306a456b48eecb9e4b72bf2
samuelaragaw1 <samuelaragaw87@gmail.com>
2025-10-05 00:01:40 +0300

    finished the flying shoe

--- Main.js
+++ Main.js
@@ -1,5 +1,14 @@
-
+const worker = new Worker("worker.js");
 document.addEventListener('DOMContentLoaded', function() {
+    let element_1 = document.querySelector("#newarrival");
+    let element_2 = document.querySelector("#shop_by_catagory");
+    let element_3 = document.querySelector("#our_story");
+    let element_4 = document.querySelector("#shoe_1");
+    let element_5 = document.querySelector("#shoe_2");
+    /*distance for flying shoes */
+    // console.log("===")
+    /*==============================================================*/
+    // let re = element.getBoundingClientRect();
     let slide = true;
     let scrolled = 0;
     let fav_click = 0;
@@ -17,13 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
                     counter.style.width = '1.6rem';
                     counter.style.paddingLeft = '0.7%';
                 }
-    }
+            }
     );
     function scrollFunction() {
         let header = document.querySelector('header');
         let bestsellers = document.querySelector('#bestsellers');
         let scroll_Y = window.scrollY;
-        console.log(scroll_Y)
+        console.log(scroll_Y);
 
         /*this for cheaking if scrolled down*/
         if (scroll_Y > scrolled) {
@@ -50,6 +59,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         else {
             header.style.top = "0px";
+        }
+
+        /*Making the shoe fly*/
+        if (scroll_Y >= 2803 && scroll_Y <= 3271) {
+            element_1.style.top = 691 + (scroll_Y - 2803) + "px";
+            element_2.style.top = 1442 + (scroll_Y - 2803) + "px";
+            element_3.style.top = 2122 + (scroll_Y - 2803) + "px";
+            /*The shoe*/
+            // console.log("===================================");
+            // console.log(d_4 - 1000 - (scroll_Y - 2661) + "px");
+            // console.log(d_5 - 1000 - (scroll_Y - 2661) + "px");
+            // console.log("===================================");
+            element_4.style.top = 498 - (scroll_Y - 2803) + "px";
+            element_5.style.top = 634 - (scroll_Y- 2803) + "px";
         }
     }
     function control_slid() {
